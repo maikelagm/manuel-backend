d559066cfaf19e1ba12f3a6dfdfaac1431bc85f7
José Javier Quesada Madrigal <xocechka@gmail.com>
2023-12-02 17:08:41 -0500

    Add offline docs with python-fastapioffline

--- app/main.py
+++ app/main.py
@@ -1,7 +1,13 @@
+import uvicorn
 from fastapi import FastAPI
+from fastapi_offline import FastAPIOffline
+
 from router import connection_routes, scann_routes
 
-app = FastAPI()
+app = FastAPIOffline()
 
 app.include_router(connection_routes.connection, prefix="/connection", tags=["database"])
 app.include_router(scann_routes.router, prefix="/scan", tags=["security"])
+
+if __name__ == "__main__":
+    uvicorn.run("app.main:app", host="127.0.0.1", port=8000, reload=True)
